db6dad692b9c71a3afeef2f3a059a52fc29e1300
harshavardhan <thisisharshavardhan@gmail.com>
2024-06-21 21:15:56 +0530

    make a Login page

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Login from './Components/Login_and_registration/Login.jsx'
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/Login_and_registration/Login.jsx
@@ -0,0 +1,40 @@
+import './Login.css'
+import google_logo from '../../Assets/google_logo.svg'
+import github_logo from '../../Assets/github_logo.svg'
+
+function Login() {
+    return (
+        <>
+            <div className="login_outer_div">
+                <h2 className='login_title'>
+                    Login to <h4>Project Ideas</h4>
+                </h2>
+                <div className="login_input_div">
+                    <label className='login_label'>Email</label>
+                    <input type="email" className='login_input' placeholder='user@domain' />
+                </div>
+                <div className="login_input_div">
+                    <label className='login_label'>Password</label>
+                    <input type="password" className='login_input' placeholder='password' />
+                </div>
+                <button className='login_button'>Login</button>
+                {/* login using google or github */}
+                <div className="social_login">
+                    <div className="with_google">
+                        <img src={google_logo} className='google_logo '></img>
+                        <p>Continue with Google</p>
+                    </div>
+                    <div className="with_github">
+                        <img src={github_logo} className='github_logo '></img>
+                        <p>Continue with Github</p>
+                    </div>
+                </div>
+                <div className="login_footer">
+                    <p className='dont_have_account'>Don&apos;t have an account? <a href='#'>Register</a></p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
